d9db1568847f557ba5ec36c77c456718759dfcdc
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-09-28 20:53:41 +0530

    minor change before deploy

--- node_backend/app.js
+++ node_backend/app.js
@@ -32,7 +32,7 @@ app.use(cors({
     credentials: true,
 }))
 
-app.get("/",(req, res) => {
+app.get("/test",(req, res) => {
     res.status(201).json({
         message: "Hey , Your backend is running",
     })
